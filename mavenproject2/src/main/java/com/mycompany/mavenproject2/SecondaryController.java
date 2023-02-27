f85090288fd7f1705095157fa27b2d5b849bef6e
George-Couldy <94755687+George-Couldy@users.noreply.github.com>
2023-02-27 16:58:17 +0200

    Add files via upload

---
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/SecondaryController.java
@@ -0,0 +1,229 @@
+package com.mycompany.mavenproject2;
+
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class SecondaryController {
+
+    @FXML
+    private TextField color1;
+    @FXML
+    private TextField color3;
+    @FXML
+    private TextField color2;
+    @FXML
+    private Label label;
+    @FXML
+    private Label les1;
+    @FXML
+    private Label les2;
+    @FXML
+    private Label les3;
+    @FXML
+    private Label lesName;
+    @FXML
+    private Label bot1;
+    @FXML
+    private Label bot2;
+    @FXML
+    private Label bot3;
+    @FXML
+    private Label botName;
+    @FXML
+    private Label egy1;
+    @FXML
+    private Label egy2;
+    @FXML
+    private Label egy3;
+    @FXML
+    private Label egyName;
+    @FXML
+    private Label gha1;
+    @FXML
+    private Label gha2;
+    @FXML
+    private Label gha3;
+    @FXML
+    private Label ghaName;
+    @FXML
+    private Label aus1;
+    @FXML
+    private Label aus2;
+    @FXML
+    private Label aus3;
+    @FXML
+    private Label ausName;
+
+    @FXML
+    private void lesotho(ActionEvent event) {
+        color1.setStyle("-fx-background-color:blue");
+        color2.setStyle("-fx-background-color:white");
+        color3.setStyle("-fx-background-color:green");
+        label.setText("LESOTHO");
+        les1.setStyle("-fx-background-color:");
+        les2.setStyle("-fx-background-color:");
+        les3.setStyle("-fx-background-color:");
+        lesName.setText("");
+        bot1.setStyle("-fx-background-color:");
+        bot2.setStyle("-fx-background-color:");
+        bot3.setStyle("-fx-background-color:");
+        botName.setText("");
+        aus1.setStyle("-fx-background-color:");
+        aus2.setStyle("-fx-background-color:");
+        aus3.setStyle("-fx-background-color:");
+        ausName.setText("");
+        gha1.setStyle("-fx-background-color:");
+        gha2.setStyle("-fx-background-color:");
+        gha3.setStyle("-fx-background-color:");
+        ghaName.setText("");
+        egy1.setStyle("-fx-background-color:");
+        egy2.setStyle("-fx-background-color:");
+        egy3.setStyle("-fx-background-color:");
+        egyName.setText("");
+    }
+
+    @FXML
+    private void botswana(ActionEvent event) {
+        color1.setStyle("-fx-background-color:lightblue");
+        color2.setStyle("-fx-background-color:white");
+        color3.setStyle("-fx-background-color:lightblue");
+        label.setText("BOTSWANA");
+        les1.setStyle("-fx-background-color:");
+        les2.setStyle("-fx-background-color:");
+        les3.setStyle("-fx-background-color:");
+        lesName.setText("");
+        bot1.setStyle("-fx-background-color:");
+        bot2.setStyle("-fx-background-color:");
+        bot3.setStyle("-fx-background-color:");
+        botName.setText("");
+        aus1.setStyle("-fx-background-color:");
+        aus2.setStyle("-fx-background-color:");
+        aus3.setStyle("-fx-background-color:");
+        ausName.setText("");
+        gha1.setStyle("-fx-background-color:");
+        gha2.setStyle("-fx-background-color:");
+        gha3.setStyle("-fx-background-color:");
+        ghaName.setText("");
+        egy1.setStyle("-fx-background-color:");
+        egy2.setStyle("-fx-background-color:");
+        egy3.setStyle("-fx-background-color:");
+        egyName.setText("");
+    }
+
+
+    @FXML
+    private void austria(ActionEvent event) {
+        color1.setStyle("-fx-background-color:red");
+        color2.setStyle("-fx-background-color:white");
+        color3.setStyle("-fx-background-color:red");
+        label.setText("AUSTRIA");
+        les1.setStyle("-fx-background-color:");
+        les2.setStyle("-fx-background-color:");
+        les3.setStyle("-fx-background-color:");
+        lesName.setText("");
+        bot1.setStyle("-fx-background-color:");
+        bot2.setStyle("-fx-background-color:");
+        bot3.setStyle("-fx-background-color:");
+        botName.setText("");
+        aus1.setStyle("-fx-background-color:");
+        aus2.setStyle("-fx-background-color:");
+        aus3.setStyle("-fx-background-color:");
+        ausName.setText("");
+        gha1.setStyle("-fx-background-color:");
+        gha2.setStyle("-fx-background-color:");
+        gha3.setStyle("-fx-background-color:");
+        ghaName.setText("");
+        egy1.setStyle("-fx-background-color:");
+        egy2.setStyle("-fx-background-color:");
+        egy3.setStyle("-fx-background-color:");
+        egyName.setText("");
+    }
+
+    @FXML
+    private void ghana(ActionEvent event) {
+        color1.setStyle("-fx-background-color:red");
+        color2.setStyle("-fx-background-color:yellow");
+        color3.setStyle("-fx-background-color:green");
+        label.setText("GHANA");
+        les1.setStyle("-fx-background-color:");
+        les2.setStyle("-fx-background-color:");
+        les3.setStyle("-fx-background-color:");
+        lesName.setText("");
+        bot1.setStyle("-fx-background-color:");
+        bot2.setStyle("-fx-background-color:");
+        bot3.setStyle("-fx-background-color:");
+        botName.setText("");
+        aus1.setStyle("-fx-background-color:");
+        aus2.setStyle("-fx-background-color:");
+        aus3.setStyle("-fx-background-color:");
+        ausName.setText("");
+        gha1.setStyle("-fx-background-color:");
+        gha2.setStyle("-fx-background-color:");
+        gha3.setStyle("-fx-background-color:");
+        ghaName.setText("");
+        egy1.setStyle("-fx-background-color:");
+        egy2.setStyle("-fx-background-color:");
+        egy3.setStyle("-fx-background-color:");
+        egyName.setText("");
+    }
+
+    @FXML
+    private void egypt(ActionEvent event) {
+        color1.setStyle("-fx-background-color:red");
+        color2.setStyle("-fx-background-color:white");
+        color3.setStyle("-fx-background-color:purple");
+        label.setText("EGYPT");
+        les1.setStyle("-fx-background-color:");
+        les2.setStyle("-fx-background-color:");
+        les3.setStyle("-fx-background-color:");
+        lesName.setText("");
+        bot1.setStyle("-fx-background-color:");
+        bot2.setStyle("-fx-background-color:");
+        bot3.setStyle("-fx-background-color:");
+        botName.setText("");
+        aus1.setStyle("-fx-background-color:");
+        aus2.setStyle("-fx-background-color:");
+        aus3.setStyle("-fx-background-color:");
+        ausName.setText("");
+        gha1.setStyle("-fx-background-color:");
+        gha2.setStyle("-fx-background-color:");
+        gha3.setStyle("-fx-background-color:");
+        ghaName.setText("");
+        egy1.setStyle("-fx-background-color:");
+        egy2.setStyle("-fx-background-color:");
+        egy3.setStyle("-fx-background-color:");
+        egyName.setText("");
+    }
+    
+    @FXML
+    private void reset(ActionEvent event) {
+        color1.setStyle("-fx-background-color:pink");
+        color2.setStyle("-fx-background-color:pink");
+        color3.setStyle("-fx-background-color:pink");
+        label.setText("");
+        les1.setStyle("-fx-background-color:blue");
+        les2.setStyle("-fx-background-color:white");
+        les3.setStyle("-fx-background-color:green");
+        lesName.setText("LESOTHO");
+        bot1.setStyle("-fx-background-color:lightblue");
+        bot2.setStyle("-fx-background-color:white");
+        bot3.setStyle("-fx-background-color:lightblue");
+        botName.setText("BOTSWANA");
+        aus1.setStyle("-fx-background-color:red");
+        aus2.setStyle("-fx-background-color:white");
+        aus3.setStyle("-fx-background-color:red");
+        ausName.setText("AUSTRIA");
+        gha1.setStyle("-fx-background-color:red");
+        gha2.setStyle("-fx-background-color:yellow");
+        gha3.setStyle("-fx-background-color:green");
+        ghaName.setText("GHANA");
+        egy1.setStyle("-fx-background-color:red");
+        egy2.setStyle("-fx-background-color:white");
+        egy3.setStyle("-fx-background-color:purple");
+        egyName.setText("EGYPT");
+    }
+}
